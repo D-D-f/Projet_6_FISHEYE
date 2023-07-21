51cc2ee4aa6c23e3188ef88385920a6c1a924051
David de Freitas <daviddefreitas@iMac-de-David.local>
2023-07-21 15:12:56 +0200

    update css

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,24 @@
 const photographerTemplate = (data) => {
-  const { name, portrait } = data;
-
+  const { name, portrait, city, country, price, tagline } = data;
   const picture = `assets/photographers/${portrait}`;
 
   function getUserCardDOM() {
     const article = document.createElement("article");
+    const a = document.createElement("a");
     const img = document.createElement("img");
+    const spanReside = document.createElement("span");
+    const spanCitation = document.createElement("span");
+    const spanPrice = document.createElement("span");
     img.setAttribute("src", picture);
-    img.style.borderRadius = "50%";
+    a.setAttribute("href", "#");
     const h2 = document.createElement("h2");
     h2.textContent = name;
-    article.appendChild(img);
-    article.appendChild(h2);
+    spanReside.textContent = `${city}, ${country}`;
+    spanCitation.textContent = tagline;
+    spanPrice.textContent = `${price}€/ jour`;
+    article.appendChild(a);
+    a.appendChild(img);
+    a.append(h2, spanReside, spanCitation, spanPrice);
     return article;
   }
   return { name, picture, getUserCardDOM };
