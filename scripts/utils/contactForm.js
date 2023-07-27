2d98658e734bb95ccff4e1874a1b3cc1a4c3f419
D-D-f <defreitas-david@outlook.fr>
2023-07-28 00:03:10 +0200

    formulaire ok

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -11,3 +11,20 @@ function closeModal() {
   pages.style.display = "block";
   modal.style.display = "none";
 }
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  const formValues = {};
+
+  for (const [name, value] of formData.entries()) {
+    formValues[name] = value;
+  }
+
+  console.log(formValues);
+  closeModal();
+});
